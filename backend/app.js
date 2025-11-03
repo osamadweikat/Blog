2fc775113f6b018552ca0edbfc4feb4a7bbcafaa
osamadweikat <osamadweikat83@gmail.com>
2025-11-03 18:50:40 +0200

    first commit

---
+++ backend/app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const connectToDb = require("./config/connectToDb");
+require("dotenv").config();
+
+connectToDb();
+
+const app = express();
+
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () =>
+  console.log(
+    `Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`
+  )
+);
