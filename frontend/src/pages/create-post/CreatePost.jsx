bd87125bdd83a41c56acd4fd8c001accab332095
osamadweikat <osamadweikat83@gmail.com>
2025-11-09 17:12:29 +0200

    feat: add create post component with styling

--- frontend/src/pages/create-post/CreatePost.jsx
+++ frontend/src/pages/create-post/CreatePost.jsx
@@ -1,5 +1,39 @@
+import "./create-post.css";
+
 const CreatePost = () => {
-  return <h1>Create Post Page</h1>;
+  return (
+    <section className="create-post">
+      <h1 className="create-post-title">Create New Post</h1>
+      <form className="create-post-form">
+        <input
+          type="text"
+          placeholder="Post Title"
+          className="create-post-input"
+        />
+        <select className="create-post-input">
+          <option disabled value="">
+            Select A Category
+          </option>
+          <option value="music">music</option>
+          <option value="travelling">travelling</option>
+        </select>
+        <textarea
+          className="create-post-textarea"
+          placeholder="Post Description"
+          rows="5"
+        ></textarea>
+        <input
+          className="create-post-upload"
+          type="file"
+          name="file"
+          id="file"
+        />
+        <button type="submit" className="create-post-btn">
+          Create
+        </button>
+      </form>
+    </section>
+  );
 };
 
 export default CreatePost;
--- frontend/src/pages/posts-page/PostsPage.jsx
+++ frontend/src/pages/posts-page/PostsPage.jsx
@@ -3,8 +3,13 @@ import PostList from "../../components/posts/PostList";
 import Sidebar from "../../components/sidebar/Sidebar";
 import { posts, categories } from "../../dummyData";
 import Pagination from "../../components/pagination/Pagination";
+import { useEffect } from "react";
 
 const PostsPage = () => {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
   return (
     <>
       <section className="posts-page">
