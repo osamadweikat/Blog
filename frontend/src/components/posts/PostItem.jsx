a937ead40a130ec56f5c3963a4cdaa458d27dce9
osamadweikat <osamadweikat83@gmail.com>
2025-11-09 14:23:28 +0200

    feat: add Home page with PostList and PostItem components

---
+++ frontend/src/components/posts/PostItem.jsx
@@ -0,0 +1,42 @@
+import { Link } from "react-router-dom";
+
+const PostItem = ({ post }) => {
+  return (
+    <div className="post-item">
+      <div className="post-item-image-wrapper">
+        <img src={post.image} alt="" className="post-item-image" />
+      </div>
+      <div className="post-item-info-wrapper">
+        <div className="post-item-info">
+          <div className="post-item-author">
+            <strong>Author: </strong>
+            <Link to="/profile/1">{post.user.username}</Link>
+          </div>
+          <div className="post-item-date">
+            {new Date(post.createdAt).toDateString()}
+          </div>
+        </div>
+        <div className="post-item-details">
+          <h4 className="post-item-title">{post.title}</h4>
+          <Link
+            className="post-item-category"
+            to={`/posts/categories/${post.category}`}
+          >
+            {post.category}
+          </Link>
+        </div>
+        <p className="post-item-description">
+          {post.description} Lorem ipsum dolor, sit amet consectetur adipisicing
+          elit. Praesentium eos omnis id in dolorem nihil fugit reiciendis
+          voluptatem illo eius assumenda atque quibusdam molestiae, unde ducimus
+          esse quaerat debitis. Repellat?
+        </p>
+        <Link className="post-item-link" to={`/posts/detailes/${post._id}`}>
+          Read More...
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default PostItem;
--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,5 +1,22 @@
+import "./home.css";
+import PostList from "../../components/posts/PostList";
+import { posts } from "../../dummyData";
+
 const Home = () => {
-  return <h1>Home Page</h1>;
+  return (
+    <section className="home">
+      <div className="home-hero-header">
+        <div className="home-hero-header-layout">
+          <h1 className="home-title">Welcome to Blog</h1>
+        </div>
+      </div>
+      <div className="home-latest-post">Latest Posts</div>
+      <div className="home-container">
+        <PostList posts={posts} />
+        <div className="post-sidebar">Side Bar</div>
+      </div>
+    </section>
+  );
 };
 
 export default Home;
