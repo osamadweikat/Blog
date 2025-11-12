46cdea145e080d7bc5442134097b0af037797ead
osamadweikat <osamadweikat83@gmail.com>
2025-11-12 21:33:55 +0200

    Add deleteProfile action to remove user profile

--- frontend/src/redux/slices/profileSlice.js
+++ frontend/src/redux/slices/profileSlice.js
@@ -4,6 +4,8 @@ const profileSlice = createSlice({
   name: "profile",
   initialState: {
     profile: null,
+    loading: false,
+    isProfileDeleted: false,
   },
   reducers: {
     setProfile(state, action) {
@@ -14,6 +16,19 @@ const profileSlice = createSlice({
     },
     updateProfile(state, action) {
       state.profile = action.payload;
+    },
+    setLoading(state) {
+      state.loading = true;
+    },
+    clearLoading(state) {
+      state.loading = false;
+    },
+    setIsProfileDeleted(state) {
+      state.isProfileDeleted = true;
+      state.loading = false;
+    },
+    clearIsProfileDeleted(state) {
+      state.isProfileDeleted = false;
     },
   },
 });
