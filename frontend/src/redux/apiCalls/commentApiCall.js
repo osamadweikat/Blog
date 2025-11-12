c7aa42e729a7795f31f287a8c791985989170417
osamadweikat <osamadweikat83@gmail.com>
2025-11-12 20:06:18 +0200

    Add deleteComment action to remove a comment from a post

--- frontend/src/redux/apiCalls/commentApiCall.js
+++ frontend/src/redux/apiCalls/commentApiCall.js
@@ -35,3 +35,18 @@ export function updateComment(commentId, comment) {
     }
   };
 }
+
+export function deleteComment(commentId) {
+  return async (dispatch, getState) => {
+    try {
+      await request.delete(`/api/comments/${commentId}`, {
+        headers: {
+          Authorization: "Bearer " + getState().auth.user.token,
+        },
+      });
+      dispatch(postActions.deleteCommentFromPost(commentId));
+    } catch (error) {
+      toast.error(error.response.data.message);
+    }
+  };
+}
